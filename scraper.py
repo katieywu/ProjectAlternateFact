63f3c73bdcc7b414d44ed11aaaa7b37dcfa400ce
katieywu <katiewu95@gmail.com>
2017-02-07 14:27:30 -0500

    initial commit w scraper

---
+++ scraper.py
@@ -0,0 +1,32 @@
+# import some Python dependencies
+
+import urllib2
+import json
+# import datetime
+# import csv
+# import time
+
+app_id = "1175114875934600"
+app_secret = "06e3908a682c9b05c25439d25909f700"  # DO NOT SHARE WITH ANYONE!
+
+access_token = app_id + "|" + app_secret
+
+page_id = 'nytimes'
+
+
+def testFacebookPageData(page_id, access_token):
+
+    # construct the URL string
+    base = "https://graph.facebook.com/v2.8"
+    node = "/" + page_id
+    parameters = "/?access_token=" + access_token
+    url = base + node + parameters
+
+    # retrieve data
+    request = urllib2.Request(url)
+    response = urllib2.urlopen(request)
+    data = json.loads(response.read())
+
+    print json.dumps(data, indent=4, sort_Keys=True)
+
+testFacebookPageData(page_id, access_token)
