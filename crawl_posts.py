ba31b3a3411259e7b9ac2c2ede4b4af86e972f57
katieywu <katiewu95@gmail.com>
2017-02-08 14:12:31 -0500

    added modularity for post crawling

--- crawl_posts.py
+++ crawl_posts.py
@@ -3,9 +3,7 @@
 import urllib2
 import json
 import datetime
-# import csv
 import time
-import mechanize
 
 app_id = "1846851035593024"
 app_secret = "e99dd80854c0d30cc0c3bbabed5f7e98"  # TOP SECRET
@@ -14,24 +12,14 @@ access_token = app_id + "|" + app_secret
 
 page_id = 'nytimes'
 
-browser = mechanize.Browser()
-browser.set_handle_robots(False)
-browser.open("http://www.facebook.com/")
-browser.select_form(nr=0)
-print browser.form
 
-# browser.select_form(nr = 0)
-# browser.form['username'] = ""
-# browser.form['password'] = ""
-# browser.submit()
+def basic_get_request(url):
 
-
-def basic_get_request():
-
-    request = urllib2.Request("https://www.facebook.com/")
+    request = urllib2.Request(url)
+    request.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:5.0)')
     response = urllib2.urlopen(request)
 
-    print response.read()
+    print response.info()
 
 
 def test_facebook_page_data(page_id, access_token):
@@ -67,14 +55,4 @@ def request_until_succeed(url):
 
     return response.read()
 
-
-def get_facebook_page_followers(page_id, access_token):
-
-    # construct the URL string
-    base = "https://graph.facebook.com/v2.8"
-    node = "/" + page_id + "/feed"
-    parameters = "/?access_token=%s" % access_token
-    url = base + node + parameters
-
-# basic_get_request()
-# test_facebook_page_data(page_id, access_token)
+basic_get_request("http://www.facebook.com/")
