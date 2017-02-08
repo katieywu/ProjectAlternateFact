cb5042e80f6666079ac4164070ff1d0a10c6d2f9
katieywu <katiewu95@gmail.com>
2017-02-08 13:19:34 -0500

    i think i fixed it fuck yea

--- scraper.py
+++ scraper.py
@@ -2,24 +2,43 @@
 
 import urllib2
 import json
-# import datetime
+import datetime
 # import csv
-# import time
+import time
+import mechanize
 
-app_id = "1175114875934600"
-app_secret = "06e3908a682c9b05c25439d25909f700"  # DO NOT SHARE WITH ANYONE!
+app_id = "1846851035593024"
+app_secret = "e99dd80854c0d30cc0c3bbabed5f7e98"  # TOP SECRET
 
 access_token = app_id + "|" + app_secret
 
 page_id = 'nytimes'
 
+browser = mechanize.Browser()
+browser.set_handle_robots(False)
+browser.open("http://www.facebook.com/")
+browser.select_form(nr=0)
+print browser.form
 
-def testFacebookPageData(page_id, access_token):
+# browser.select_form(nr = 0)
+# browser.form['username'] = ""
+# browser.form['password'] = ""
+# browser.submit()
+
+
+def basic_get_request():
+
+    request = urllib2.Request("https://www.facebook.com/")
+    response = urllib2.urlopen(request);
+
+    print response.read();
+
+def test_facebook_page_data(page_id, access_token):
 
     # construct the URL string
     base = "https://graph.facebook.com/v2.8"
     node = "/" + page_id
-    parameters = "/?access_token=" + access_token
+    parameters = "/?access_token=%s" % access_token
     url = base + node + parameters
 
     # retrieve data
@@ -29,4 +48,32 @@ def testFacebookPageData(page_id, access_token):
 
     print json.dumps(data, indent=4, sort_keys=True)
 
-testFacebookPageData(page_id, access_token)
+
+def request_until_succeed(url):
+
+    request = urllib2.Request(url)
+    success = False
+    while success is False:
+        try:
+            response = urllib2.urlopen(request)
+            if response.getcode() == 200:
+                success = True
+        except Exception, e:
+            print e
+            time.sleep(5)
+
+            print "Error for URL %s: %s" % (url, datetime.datetime.now())
+
+    return response.read()
+
+
+def get_facebook_page_followers(page_id, access_token):
+
+    # construct the URL string
+    base = "https://graph.facebook.com/v2.8"
+    node = "/" + page_id + "/feed"
+    parameters = "/?access_token=%s" % access_token
+    url = base + node + parameters
+
+# basic_get_request()
+# test_facebook_page_data(page_id, access_token)
