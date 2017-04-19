d63343b645d74daa75d348eb4271fbb2d1c6714e
katieywu <katiewu95@gmail.com>
2017-04-19 17:48:33 -0400

    remove shelve cuz it sucks ass

--- n_set_intersections.py
+++ n_set_intersections.py
@@ -1,50 +1,58 @@
 import os
 from bsddb3 import db
-from collections import defaultdict
-import gc
+import shelve
 
 a = [(1, "a"), (2, "a"), (2, "b"), (3, "a"), (3, "b"), (1, "c"), (5, "a")]
 
+
 # # Part 1: Create database and insert 4 elements
 # #
-filename = 'fruit'
-
-# Get an instance of BerkeleyDB
-fruitDB = db.DB()
-# Create a database in file "fruit" with a Hash access method
-# 	There are also, B+tree and Recno access methods
-fruitDB.open(filename, None, db.DB_HASH, db.DB_CREATE)
-
-# Print version information
-print '\t', db.DB_VERSION_STRING
-
-# Insert new elements in database
-fruitDB.put("apple","red")
-fruitDB.put("orange","orange")
-fruitDB.put("banana","yellow")
-fruitDB.put("tomato","red")
-
-# Close database
-fruitDB.close()
-
-# Part 2: Open database and write its contents out
+# filename = 'fruit'
 #
-fruitDB = db.DB()
-# Open database
-#	Access method: Hash
-#	set isolation level to "dirty read (read uncommited)"
-fruitDB.open(filename, None, db.DB_HASH, db.DB_DIRTY_READ)
+# # Get an instance of BerkeleyDB
+# mapDB = db.DB()
+# # Create a database in file "fruit" with a Hash access method
+# # 	There are also, B+tree and Recno access methods
+# mapDB.open(filename, None, db.DB_HASH, db.DB_CREATE)
+#
+# # Print version information
+# print '\t', db.DB_VERSION_STRING
+#
+# # Insert new elements in database
+# mapDB.put("apple", ["red", "blue"])
+# mapDB.put("orange", "orange")
+# mapDB.put("banana", "yellow")
+# mapDB.put("tomato", "red")
+#
+# cursor = mapDB.cursor()
+# rec = cursor.first()
+# while rec:
+#     print rec
+#     rec = cursor.next()
+#
+# # Close database
+# mapDB.close()
 
-# get database cursor and print out database content
-cursor = fruitDB.cursor()
-rec = cursor.first()
-while rec:
-        print rec
-        rec = cursor.next()
-fruitDB.close()
+# # Part 2: Open database and write its contents out
+# #
+# fruitDB = db.DB()
+# # Open database
+# #	Access method: Hash
+# #	set isolation level to "dirty read (read uncommited)"
+# fruitDB.open(filename, None, db.DB_HASH, db.DB_DIRTY_READ)
+#
+# # get database cursor and print out database content
+# cursor = fruitDB.cursor()
+# rec = cursor.first()
+# while rec:
+#         print rec
+#         rec = cursor.next()
+# fruitDB.close()
 
 
 def n_set_intersection():
+
+    file_dir = "/data-no-cnn"
 
     mapping = {}
     with open("seeds.txt", "r") as my_file:
@@ -53,14 +61,14 @@ def n_set_intersection():
             mapping[temp[0]] = temp[1]
 
     # first mapping: emit a huge list #
-    file_list = os.listdir("../data/data-no-cnn/")
+    file_list = os.listdir("../data" + file_dir)
 
     starting_list = []
     for filename in file_list:
         user_id = mapping[filename.split('_')[0]]
         # user_id = filename.split('_')[0]
 
-        filename = "../data/data-no-cnn/" + filename
+        filename = "../data" + file_dir + "/" + filename
         with open(filename, "r") as infile:
             for line in infile:
                 starting_list.append((line.rstrip('\n'), user_id))
@@ -104,6 +112,6 @@ def n_set_intersection():
     return dic
 
 
-# returned = n_set_intersection()
-# print returned
+returned = n_set_intersection()
+print returned
 
