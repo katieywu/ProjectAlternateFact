54e8fdfb19106b4dad4b9e337b4c663c24d55707
katieywu <katiewu95@gmail.com>
2017-04-27 01:34:37 -0400

    fixed no overlap and gravity charge force in d3

--- d3/script.js
+++ d3/script.js
@@ -1,34 +1,43 @@
+
 //Constants for the SVG
 var width = window.innerWidth,
     height = window.innerHeight;
 
-console.log(width);
-console.log(height);
-
 //Set up the colour scale
 var color = d3.scale.category20();
 
-//Set up the force layout
-var force = d3.layout.force()
-    .charge(-120)
-    .linkDistance(80)
-    .size([width, height]);
+var zoom = d3.behavior.zoom()
+  .scaleExtent([1, 10])
+  .on("zoom", zoomed);
 
 //Append a SVG to the body of the html page. Assign this SVG as an object to svg
 var svg = d3.select("body").append("svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    .call(zoom)
+    .append('svg:g');
+
 
 //Read the data from the data element 
 //var data = document.getElementById('data').innerHTML;
 //console.log(data)
 graph = data;
 
+var k = Math.sqrt(graph.nodes.length / (width * height));
+//Set up the force layout
+var force = d3.layout.force()
+//    .charge(-1000)
+    .charge(-10 / k)
+    .gravity(100 * k)
+    .linkDistance(80)
+    .size([width, height]);
     
 //Creates the graph data structure out of the json data
 force.nodes(graph.nodes)
     .links(graph.links)
     .start();
+
+force.drag().on("dragstart", function() { d3.event.sourceEvent.stopPropagation(); });
 
 //Create all the line svgs but without locations yet
 var link = svg.selectAll(".link")
@@ -63,11 +72,11 @@ node.append("text")
       .attr("dx", 10)
       .attr("dy", ".35em")
       .text(function(d) { return d.name });
-//End changed
 
 
 //Now we are giving the SVGs co-ordinates - the force layout is generating the co-ordinates which this code is using to update the attributes of the SVG elements
 force.on("tick", function () {
+    
     link.attr("x1", function (d) {
         return d.source.x;
     })
@@ -98,6 +107,97 @@ force.on("tick", function () {
     });
     
     //End Changed
+//      var q = d3.geom.quadtree(graph.nodes),
+//  i = 0,
+//  n = graph.nodes.length;
+//    while (++i < n) q.visit(collide(graph.nodes[i]), 0.5);
+    node.each(collide(0.5)); //Added
 
 });
 
+
+function zoomed() {
+  svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+}
+
+function dragstarted(d) {
+  d3.event.sourceEvent.stopPropagation();
+
+  d3.select(this).classed("dragging", true);
+  force.start();
+}
+
+function dragged(d) {
+
+  d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
+
+}
+
+function dragended(d) {
+
+  d3.select(this).classed("dragging", false);
+}
+
+var padding = 16;
+
+function collide(alpha) {
+  var quadtree = d3.geom.quadtree(graph.nodes);
+  return function(d) {
+    var radius;
+    if (d.size * 0.00001 < 1){
+        radius = 3;
+    } else {
+        radius = d.size * 0.00001
+    }
+//      radius = 5;
+    var rb = radius*2 + padding,
+        nx1 = d.x - rb,
+        nx2 = d.x + rb,
+        ny1 = d.y - rb,
+        ny2 = d.y + rb;
+    quadtree.visit(function(quad, x1, y1, x2, y2) {
+      if (quad.point && (quad.point !== d)) {
+        var x = d.x - quad.point.x,
+            y = d.y - quad.point.y,
+            l = Math.sqrt(x * x + y * y);
+          if (l < rb) {
+          l = (l - rb) / l * alpha;
+          d.x -= x *= l;
+          d.y -= y *= l;
+          quad.point.x += x;
+          quad.point.y += y;
+        }
+      }
+      return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+    });
+  };
+}
+//function collide(node, alpha) {
+////    console.log(node.radius);
+//    var radius;
+//    if (node.size * 0.00001 < 1){
+//        radius = 3;
+//    } else {
+//        radius = node.size * 0.00001
+//    }
+//  var rb = radius + 16,
+//      nx1 = node.x - rb,
+//      nx2 = node.x + rb,
+//      ny1 = node.y - rb,
+//      ny2 = node.y + rb;
+//  return function(quad, x1, y1, x2, y2) {
+//    if (quad.point && (quad.point !== node)) {
+//        var x = node.x - quad.point.x,
+//            y = node.y - quad.point.y,
+//            l = Math.sqrt(x * x + y * y);
+//          if (l < rb) {
+//          l = (l - rb) / l * alpha;
+//          node.x -= x *= l;
+//          node.y -= y *= l;
+//          quad.point.x += x;
+//          quad.point.y += y;
+//      }
+//    }
+//    return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+//  };
+//}
